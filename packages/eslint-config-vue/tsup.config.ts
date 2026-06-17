a3e37c3e9b55978a2ba3256c17190cf2615960e1
Robert Garrison <robertellisgarrison@gmail.com>
2026-06-17 10:11:37 -0400

    feat: add @midden/eslint-config-vue package, closes #6

---
+++ packages/eslint-config-vue/tsup.config.ts
@@ -0,0 +1,12 @@
+// /packages/eslint-config-vue/tsup.config.ts
+
+import { defineConfig } from "tsup";
+
+export default defineConfig({
+  entry: ["src/index.ts"],
+  format: ["esm"],
+  dts: true,
+  sourcemap: true,
+  clean: true,
+  target: "es2022",
+});
